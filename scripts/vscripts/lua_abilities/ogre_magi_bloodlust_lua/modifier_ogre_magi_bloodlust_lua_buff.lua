01dbb0d254eb4b39900993665528d8d4e5cbb795
pvp-by <kain@pvp.by>
2022-04-16 18:05:08 +0400

    Update modifier_ogre_magi_bloodlust_lua_buff.lua
    I found a small error in your code

--- scripts/vscripts/lua_abilities/ogre_magi_bloodlust_lua/modifier_ogre_magi_bloodlust_lua_buff.lua
+++ scripts/vscripts/lua_abilities/ogre_magi_bloodlust_lua/modifier_ogre_magi_bloodlust_lua_buff.lua
@@ -72,10 +72,10 @@ function modifier_ogre_magi_bloodlust_lua_buff:DeclareFunctions()
 end
 
 function modifier_ogre_magi_bloodlust_lua_buff:GetModifierMoveSpeedBonus_Percentage()
-	return self.as_bonus
+	return self.ms_bonus
 end
 function modifier_ogre_magi_bloodlust_lua_buff:GetModifierAttackSpeedBonus_Constant()
-	return self.ms_bonus
+	return self.as_bonus
 end
 
 function modifier_ogre_magi_bloodlust_lua_buff:GetModifierModelScale()
